a9d8c6e8f3d7cf26faa266af69bd8b9d49f988c6
CodingEnthusiast <CodingEnthusiast@protonmail.com>
2019-03-29 19:39:37 +0430

    Added comments for clarity.

--- AddrConverter/Program.cs
+++ AddrConverter/Program.cs
@@ -11,14 +11,18 @@ namespace AddrConverter
             while (true)
             {
                 Console.WriteLine("Please enter your P2PKH address (should start with 1):");
-                string temp = Console.ReadLine();
-                if (!string.IsNullOrWhiteSpace(temp) && temp[0] == '1' && enc.IsValid(temp))
+                string addr = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(addr) && addr[0] == '1' && enc.IsValid(addr))
                 {
-                    byte[] hash160 = enc.DecodeWithCheckSum(temp);
+                    // A P2PKH is Base58 encoding of Hash160 hash of public key with a version byte in the beginning
+                    byte[] hash160 = enc.DecodeWithCheckSum(addr);
                     if (hash160.Length != 21)
                     {
                         continue;
                     }
+
+                    // A P2SH-P2WPKH address is the Hash160 hash of the "witness script".
+                    // In this version the script is { witver || size || hash160_of_pubkey }
                     byte[] witnessScript = new byte[] { 0x00, 0x14 }.ConcatFast(hash160.SubArray(1, 20));
                     byte[] result;
                     using (RIPEMD160 rip = new RIPEMD160Managed())
@@ -30,6 +34,8 @@ namespace AddrConverter
                     }
 
                     Console.WriteLine("Your P2SH-P2WPKH address is:");
+                    // The address is encoded using Base58 encoding with a 4 byte checksum 
+                    // and the version byte is the same as P2SH addresses (=5)
                     Console.WriteLine(enc.EncodeWithCheckSum(result.AppendToBeginning(5)));
 
                     break;
